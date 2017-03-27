db400021e760190f68d291d776ffee808a185a7d
Yuriy Pankiv <pankiv97@gmail.com>
2017-03-27 16:03:42 +0300

    Visual fixes

--- src/components/history/HistoryCollection.js
+++ src/components/history/HistoryCollection.js
@@ -4,13 +4,16 @@ import { HistoryItem } from './HistoryItem';
 export const HistoryCollection = (props) => {
   
   return (
-      <div className="row">
-        {props.history.map((gift, id) => 
+      <div className="row center">
+        {props.history.length?props.history.map((gift, id) => 
           <div onClick={()=>{props.generateOldGifts(id)}} key={id}>
             <HistoryItem {...gift} index={id} 
             	deleteFromHistoryHandler={props.deleteFromHistoryHandler}/>
           </div> 
-        )}
+        ):<div className="center">
+        		<img src="./images/gift.png" className="helper-my-image-120"/>
+        		<p>No history right now...</p>
+        	</div>}
       </div>
   )
   
--- src/components/history/HistoryItem.js
+++ src/components/history/HistoryItem.js
@@ -8,15 +8,15 @@ export const HistoryItem = (props) => {
         <img src={props.picture} /> 
       </div>
     	<div className="col s12 m9 row">
-            <div className="col s12 m11">
-              <p>You wanted to find present for <b>{props.name}</b>. It's 
-              {props.gender=="male"? " his" : " her"} <b>{props.age}</b>'s anniversary!
-              You were ready to spend <b>{props.price}$</b> on a present</p>
-              <p><b>{props.tags.length?"Your friend likes "+ props.tags.reduce((a, b) => a + ", " +b):"Your friend has no interests"}</b></p>
-            </div>
-            <div className="col s12 m1 center">
-              <i className="material-icons" onClick={(e)=>{e.stopPropagation();props.deleteFromHistoryHandler(props.index)}}>delete</i>
-            </div>
+        <div className="col s12 m11">
+          <p>You wanted to find present for <b>{props.name}</b>. It's 
+          {props.gender=="male"? " his" : " her"} <b>{props.age}</b>'s anniversary!
+          You were ready to spend <b>{props.price}$</b> on a present</p>
+          <p><b>{props.tags.length?"Your friend likes "+ props.tags.reduce((a, b) => a + ", " +b):"Your friend has no interests"}</b></p>
+        </div>
+        <div className="col s12 m1 center">
+          <i className="material-icons" onClick={(e)=>{e.stopPropagation();props.deleteFromHistoryHandler(props.index)}}>delete</i>
+        </div>
     	</div>
     </div>
   )
