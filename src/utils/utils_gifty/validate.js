401dada21c8fb1156a6895a7f4f04aaefc57bcca
kshivan27 <kshanovskyy@ukr.net>
2017-03-27 20:32:16 +0300

    Add files via upload
    changed validation

--- src/utils/utils_gifty/validate.js
+++ src/utils/utils_gifty/validate.js
@@ -5,11 +5,21 @@ export const validate = values => {
   } else if (values.name.length > 15) {
     errors.name = 'Must be 15 characters or less'
   }
-  if (!values.age) {
-    errors.age = 'Required'
-  } 
+  
+  if (!values.age)  {
+     errors.age =`Required`
+  } else if (+(values.age) > 110) {
+    errors.age = "Are you sure? It's too much"
+  }
+
   if (!values.price) {
     errors.price = 'Required'
+  } else if(+(values.price) === 0) {
+    errors.price = "Enter a positive value, please"
+  }
+    
+  if((!values.male)&&(!values.female)) {
+    errors.female ="Check the gender, please"
   }
   return errors
 }
