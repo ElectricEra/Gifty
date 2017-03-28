76ec95f9e33e740ad9006dd5e14c69e279b27042
iuriidanylevych <iuriidanylevych@ukr.net>
2017-03-28 18:41:06 +0300

    server filter change

--- src/server.modules/filter.js
+++ src/server.modules/filter.js
@@ -38,7 +38,9 @@ function modObjForQueryToDB (obj) {
 
 
 
+
 function filterGeneratedIdeas (criteriaForm, generatedIdeas, n){
+    
     function quantityEqual (arr_1, arr_2) {
         var quantity = 0;
         arr_1.forEach(function (item) {
@@ -48,7 +50,11 @@ function filterGeneratedIdeas (criteriaForm, generatedIdeas, n){
         });
         return quantity
     }
-
+    
+    function randomSort (a,b) {
+        return Math.random() - 0.5;
+    }
+    
     generatedIdeas.forEach( function(item, index) {
         item.equal = quantityEqual(criteriaForm['tags'], item['tags'])
     });
@@ -58,10 +64,10 @@ function filterGeneratedIdeas (criteriaForm, generatedIdeas, n){
     
     if (n === undefined || n > generatedIdeas.length || n <= 0) {
         n = generatedIdeas.length
-    }
+    }   
     var filterArr = [];
     var count = sortArr.length <= n ? sortArr.length : n;
-    var equal = sortArr[0].equal;
+    var equal = sortArr[0].equal; 
     while (equal > 0) {
         var k = 0; 
         while (sortArr[k] !== undefined && sortArr[k].equal == equal ) {
@@ -69,24 +75,54 @@ function filterGeneratedIdeas (criteriaForm, generatedIdeas, n){
         } 
         if (k < count) {
             var partArr = sortArr.splice(0, k); 
-            partArr.sort(function (a, b) {
-                return Math.random() - 0.5;
-            });
+            partArr.sort(randomSort);
             filterArr = filterArr.concat(partArr);
             count -= k;
         }
         else {
             var partArr = sortArr.slice(0, k);
-            partArr.sort(function (a, b) {
-                return Math.random() - 0.5;
-            });
+            partArr.sort(randomSort);
             var cutArr = partArr.slice(0, count);
             filterArr = filterArr.concat(cutArr);
         }
         equal -= 1;  
     }
+    var restArr = sortArr;
+    filterArr.forEach(function(item, index){
+        let k = sortArr.indexOf(item);
+        if (k > -1) {
+            restArr.splice(k, 1)
+        }
+    });
+    restArr.sort(randomSort);
+    criteriaForm['tags'].sort(randomSort);
+    var copyOfRestArr = restArr.slice(); 
+    filterArr.forEach(function(item, index){
+        if (item['tags'].length >= item.equal && criteriaForm['tags'].length === item.equal) {
+            if( Math.random() > 0.33 && copyOfRestArr[index] !== undefined && copyOfRestArr[index].equal > 0) {
+                filterArr[index] = copyOfRestArr[index]; 
+                copyOfRestArr.splice(index,1)
+            }
+        }
+        else if (item['tags'].length > item.equal && criteriaForm['tags'].length > item.equal) {
+            var found = false;
+            for (var j = 0; j <= criteriaForm['tags'].length; j++) {
+                for (var i = 0; i <= restArr.length; i++) {
+                    if (restArr[i]['tags'].indexOf(criteriaForm['tags'][j]) > -1 ) {
+                         filterArr[index] = restArr[i]; 
+                         restArr.splice(i, 1);
+                         found  = true;
+                         break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            } 
+        }
+    }); 
     return filterArr
-};   
+};
 
 module.exports = {
     modCriteriaFormToObj: modCriteriaFormToObj,
